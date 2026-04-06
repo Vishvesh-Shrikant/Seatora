c6db51c59ffd488e9a75cacd15cf644b18d78c0d
Vishvesh-Shrikant <vishveshshri@gmail.com>
2026-04-06 22:47:46 +0530

    Added Authorization...

--- event-booking/src/main/java/com/vishvesh/event_booking/security/config/SecurityConfig.java
+++ event-booking/src/main/java/com/vishvesh/event_booking/security/config/SecurityConfig.java
@@ -67,6 +67,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -78,6 +79,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
